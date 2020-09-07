6282de17e1638c25e858a5cc53c41ee8c4c5f544
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-09-08 03:17:52 +0900

    Initial Commit

---
+++ Swift5 Login and SignUP ViewTests/Swift5_Login_and_SignUP_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift5_Login_and_SignUP_ViewTests.swift
+//  Swift5 Login and SignUP ViewTests
+//
+//  Created by 平林宏淳 on 2020/09/08.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import XCTest
+@testable import Swift5_Login_and_SignUP_View
+
+class Swift5_Login_and_SignUP_ViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
