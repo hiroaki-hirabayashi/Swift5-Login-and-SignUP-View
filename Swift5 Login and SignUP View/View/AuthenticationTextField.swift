802b96754573c2f3f1d05ec385afed0f9be34ac8
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-09-09 02:59:56 +0900

    TextFildにアンダーライン追加

---
+++ Swift5 Login and SignUP View/View/AuthenticationTextField.swift
@@ -0,0 +1,35 @@
+//
+//  LoginTextField.swift
+//  Swift5 Login and SignUP View
+//
+//  Created by 平林宏淳 on 2020/09/09.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationTextField: UITextField {
+    
+    //MARK: - LifeCycle
+                              //パウワードを隠すか隠さないか
+    init(placeholder: String, isSequredField: Bool? = false) {
+        super.init(frame: .zero)
+        self.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor.lightGray])
+        self.isSecureTextEntry = isSequredField!
+        self.textColor = .lightGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Helpers
+    
+    //TextFildにアンダーラインを追加する
+    override func draw(_ rect: CGRect) {
+        let border = CALayer()
+        border.frame = CGRect(x: rect.origin.x, y: rect.height - 0.75, width: rect.width, height: 0.75)
+        border.backgroundColor = UIColor.lightGray.cgColor
+        layer.addSublayer(border)
+    }
+}
--- Swift5 Login and SignUP View/View/LoginView.swift
+++ Swift5 Login and SignUP View/View/LoginView.swift
@@ -11,11 +11,25 @@ import UIKit
 class LoginView: UIView {
     //MARK: - Properties
     
+    let titlelLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return label
+    }()
+    
+    let emailLabel = AuthenticationLabel(text: "email", textColor: .white)
+    let passwordLabel = AuthenticationLabel(text: "password", textColor: .white)
+    let emailTextField = AuthenticationTextField(placeholder: "アドレスを入力してください")
+    let passwordTextField = AuthenticationTextField(placeholder: "パスワードを入力してください", isSequredField: true)
+
+    
     //MARK: - LifeCycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
@@ -25,4 +39,27 @@ class LoginView: UIView {
     
     
     //MARK: - Helpers
+    
+    func configureUI() {
+        backgroundColor = #colorLiteral(red: 0.5738074183, green: 0.5655357838, blue: 0, alpha: 1)
+       
+        addSubview(titlelLabel)
+        //コードでAutoLayout
+        titlelLabel.translatesAutoresizingMaskIntoConstraints = false
+        titlelLabel.topAnchor.constraint(equalTo: topAnchor, constant: 72).isActive = true
+        titlelLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
+        
+        
+        let stack = UIStackView(arrangedSubviews: [emailLabel, emailTextField, passwordLabel, passwordTextField])
+        //stackViewを横()にするか縦(Vertical)にするか
+        stack.axis = .vertical
+        //Labelたちの隙間の間隔
+        stack.spacing = 24
+       
+        addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.topAnchor.constraint(equalTo: titlelLabel.bottomAnchor, constant: 30).isActive = true
+        stack.leftAnchor.constraint(equalTo: leftAnchor, constant: 24).isActive = true
+        stack.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
+    }
 }
